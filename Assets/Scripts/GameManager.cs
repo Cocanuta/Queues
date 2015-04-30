f4128aa22244433e111279704d0a571cdbf6a06a
Ben Cherrington <Cocanuta@gmail.com>
2015-04-30 23:09:29 +0100

    Basic GUI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,33 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
 	GameObject[] tills;
-	public GameObject customer;
+	public GameObject customerObject;
+	public GameObject staffObject;
+
+	public List<Staff> staff = new List<Staff>();
+
+	public Staff selectedStaff;
 
 	// Use this for initialization
 	void Start () {
 
-		tills = GameObject.FindGameObjectsWithTag ("Till");
+		staff.Add(new Staff(0, "Ben"));
+		staff.Add(new Staff(1, "David"));
+		staff.Add(new Staff(2, "Caryan"));
 
-		SpawnCustomer ();
-		SpawnCustomer ();
-		SpawnCustomer ();
-		SpawnCustomer ();
-		SpawnCustomer ();
+		tills = GameObject.FindGameObjectsWithTag ("Till");
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if (!selectedStaff == null)
+		{
+			if (Input.GetMouseButtonDown (0))
+			{
+				RaycastHit hitInfo = new RaycastHit();
+				bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+				if (hit)
+				{
+					if (hitInfo.transform.gameObject.tag == "Till")
+					{
+						SpawnStaff (hitInfo.transform.position);
+					}
+				}
+			}
+		}
+
+	}
+
+	void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(10, 10, 100, 100));
+		GUILayout.BeginVertical ();
+
+		GUILayout.Label ("Staff:");
+		foreach(Staff s in staff)
+		{
+			if(GUILayout.Button(s.staffName))
+			{
+				selectedStaff = s;
+			}
+		}
+
+		GUILayout.EndVertical ();
+		GUILayout.EndArea ();
+	}
+
+	public void SpawnStaff(Vector3 position)
+	{
+		Vector3 spawnLocation = position + new Vector3 (0, 0.5f, -1);
+		GameObject newStaff = Instantiate (staffObject);
+		newStaff.transform.position = spawnLocation;
 	}
 
 	public void SpawnCustomer()
 	{
 		Vector3 spawnLocation = tills [Random.Range (0, tills.Length)].transform.position + new Vector3 (14, 0.5f, 1);
-		GameObject newCustomer = Instantiate (customer);
+		GameObject newCustomer = Instantiate (customerObject);
 		newCustomer.transform.position = spawnLocation;
 	}
 }
