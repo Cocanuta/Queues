b89e868090c3ccd9eda7cbaad79d65d8c625101e
David Engles <itsdavidengles@gmail.com>
2015-04-30 22:32:18 +0100

    Constructor Update
    Till
    Staff

--- Assets/Scripts/Staff.cs
+++ Assets/Scripts/Staff.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class Staff : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public int staffID;			// The ID of the staff member.
+	public string staffName;	// The name of the staff member.
+	public float staffEnergy;	// How much energy the staff member has.
+
+	// Creates an instance of Staff.
+	public Staff (int id, string name)
+	{
+		staffID = id;
+		staffName = name;
+		staffEnergy = 100.0f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	// Sets the energy to max.
+	public void setEnergyToMax()
+	{
+		staffEnergy = 100.0f;
+	}
+
+	// Sets the energy to 0;
+	public void setEnergyToZero()
+	{
+		staffEnergy = 0.0f;
+	}
+
+	// Decreases the energy by 0.01.
+	public void decreaseEnergy()
+	{
+		staffEnergy -= 0.01f;
+	}
+
+	// Increases the energy by 0.01.
+	public void increaseEnergy()
+	{
+		staffEnergy += 0.01f;
 	}
 }
