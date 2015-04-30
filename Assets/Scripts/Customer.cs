0ca94ab5562c93c906b4f93b79159abb8aef72fe
David Engles <itsdavidengles@gmail.com>
2015-04-30 22:45:56 +0100

    Added Customer Constructor

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Customer : MonoBehaviour {
+
+	public int customerID;		// The ID of the customer.
+	public float customerMoney;	// The amount of money the customer will pay.
+
+	// Creates an instance of Customer.
+	public Customer (int id, float money)
+	{
+		customerID = id;
+		customerMoney = money;
+	}
+
+}
