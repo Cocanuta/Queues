139a64f16dc07d6580cd91f41b61a818c20b4d84
Ben Cherrington <Cocanuta@gmail.com>
2015-04-30 22:34:28 +0100

    GameManager Added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	GameObject[] tills;
+	public GameObject customer;
+
+	// Use this for initialization
+	void Start () {
+
+		tills = GameObject.FindGameObjectsWithTag ("Till");
+
+		SpawnCustomer ();
+		SpawnCustomer ();
+		SpawnCustomer ();
+		SpawnCustomer ();
+		SpawnCustomer ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SpawnCustomer()
+	{
+		Vector3 spawnLocation = tills [Random.Range (0, tills.Length)].transform.position + new Vector3 (14, 0.5f, 1);
+		GameObject newCustomer = Instantiate (customer);
+		newCustomer.transform.position = spawnLocation;
+	}
+}
